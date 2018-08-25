2bad9a9aa948ca22badf65211ebc081607a5b086
Jeffrey Rios <yefriddavid@gmail.com>
2018-08-25 19:10:27 -0300

    Mejoras

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 var moment = require('moment');
 
+let port = process.env.PORT || process.argv[2] || 8080;
+
 
 app.all('/', (req, res) => res.send('Hello World!'))
 app.get('/datetime', (req, res) => {
 	res.send(moment().format('Y-MM-DD hh:mm:ss'))
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'))
 
 
